f6722b7527514591809e178fa6e9a48b96fa043c
ahmed025566 <ahmedsalah025566@gmail.com>
2023-11-06 21:43:15 +0200

    Improve devise forms styles

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   validates :description, presence: true, length: { minimum: 5, maximum: 500 }
   validates :keywords, presence: true, length: { minimum: 5, maximum: 100 }
   has_many_attached :images
+  belongs_to :user
 end
